226108b7426355d03b4231b168601ec3d7d34af2
haseeb1233 <haseebshs@gmail.com>
2023-09-12 19:57:28 +0530

    backend

---
+++ index.js
@@ -0,0 +1,69 @@
+require("dotenv").config()
+const express = require('express');
+const axios = require('axios');
+const cors=require("cors")
+const app=express()
+const port=process.env.port
+
+
+app.use(express.json())
+app.use(cors())
+
+const apiKey =process.env.API_KEY
+const openaiEndpoint=process.env.OPENAI_URL
+
+console.log(apiKey,openaiEndpoint)
+
+// Function to interact with ChatOpenAI
+
+
+const generate = async (prompt) => {
+  try {
+    const response = await axios.post(openaiEndpoint, {
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: 'user', content: prompt }],
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${apiKey}`,
+      },
+    });
+
+   return response.data.choices[0].message.content;
+  } catch (error) {
+    console.error('Error:', error);
+    return error.message
+  }
+};
+
+
+
+  app.post("/generate-content", async(req,res)=>{
+    const {type,subject}=req.body
+
+    const prompt=`can you create  ${type} from  ${subject}`
+
+    try {
+        
+        const content= await generate(prompt);
+
+        res.send({content:content})
+
+    } catch (error) {
+        res.status(500).json({ error: 'Error processing the question' });
+    }
+
+  })
+
+
+
+  
+
+
+
+
+
+
+  app.listen(port,()=>{
+    console.log(`server is running on ${port}`)
+  })
